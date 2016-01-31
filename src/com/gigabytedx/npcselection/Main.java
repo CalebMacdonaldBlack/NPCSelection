78aa719e6808c55c3ad832a3aceb89f9fa8292fb
Caleb Macdonald Black <calebmacdonaldblack@gmail.com>
2016-01-31 11:30:20 +1000

    implemented basic functionality for creating and populating inventory on entity click. untested

--- src/com/gigabytedx/npcselection/Main.java
+++ src/com/gigabytedx/npcselection/Main.java
@@ -1,33 +1,39 @@
 package com.gigabytedx.npcselection;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.logging.Logger;
 
+import org.bukkit.configuration.InvalidConfigurationException;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import com.gigabytedx.npcselection.events.Interact;
 
-public class Main extends JavaPlugin{
+public class Main extends JavaPlugin {
+
+	private File npcMenusFile = new File(getDataFolder() + "/Data/npcMenusFile.yml");
+	private FileConfiguration npcMenusConfig = YamlConfiguration.loadConfiguration(npcMenusFile);
+
 	public void onEnable() {
 		PluginDescriptionFile pdfFile = getDescription();
 		Logger logger = getLogger();
-		registerEvents();
 		registerCommands();
-		registerConfigFile();
+		loadFiles(npcMenusFile, npcMenusConfig);
+		registerEvents();
 		logger.info(pdfFile.getName() + " has been enabled (V." + pdfFile.getVersion() + ")");
 	}
-	
-	private void registerConfigFile(){
-		
-	}
+
 	private void registerCommands() {
-		//getCommand("command").setExecutor(new commandclass(this));
+		// getCommand("command").setExecutor(new commandclass(this));
 	}
 
 	private void registerEvents() {
 		PluginManager pm = getServer().getPluginManager();
-		pm.registerEvents(new Interact(this), this);
+		pm.registerEvents(new Interact(this, npcMenusConfig), this);
 	}
 
 	public void onDisable() {
@@ -35,5 +41,30 @@ public class Main extends JavaPlugin{
 		Logger logger = getLogger();
 
 		logger.info(pdfFile.getName() + " has been disabled (V." + pdfFile.getVersion() + ")");
+	}
+
+	public void saveCustomConfig(File file, FileConfiguration fileConfig) {
+		try {
+			fileConfig.save(file);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void loadFiles(File file, FileConfiguration fileConfig) {
+		if (file.exists()) {
+			try {
+				fileConfig.load(file);
+			} catch (IOException | InvalidConfigurationException e) {
+				e.printStackTrace();
+			}
+		} else {
+			try {
+				fileConfig.save(file);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 	}
 }
--- src/com/gigabytedx/npcselection/events/Interact.java
+++ src/com/gigabytedx/npcselection/events/Interact.java
@@ -1,19 +1,76 @@
 package com.gigabytedx.npcselection.events;
 
+import java.util.List;
+import java.util.Set;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.configuration.Configuration;
+import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractAtEntityEvent;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 
 import com.gigabytedx.npcselection.Main;
 
 public class Interact implements Listener {
 	Main plugin;
-	public Interact(Main plugin) {
+	Configuration config;
+	public Interact(Main plugin, Configuration config) {
 		this.plugin = plugin;
+		this.config = config;
 	}
 	
+	@EventHandler
 	public void onEntityInteract(PlayerInteractAtEntityEvent event){
 		Player player = event.getPlayer();
-		
+		if(event.getRightClicked() instanceof LivingEntity){
+			LivingEntity entity = (LivingEntity) event.getRightClicked();
+			String npcName = entity.getCustomName();
+			Set<String> npcNames = config.getConfigurationSection("npc").getKeys(false);
+			if(npcNames.contains(npcName)){
+				player.openInventory(getInventory(config, npcName));
+			}
+		}
+	}
+	
+	private Inventory getInventory(Configuration config, String npcName) {
+		Inventory inventoryScreen = Bukkit.createInventory(null, 9, "NPC: " + npcName);
+		Set<String> list = config.getConfigurationSection("npc." + npcName + ".options").getKeys(false);
+		for (String itemData : list) {
+			ItemStack itemStack = new ItemStack(Material.valueOf(config.getString("npc." + npcName + ".options." + itemData + ".material")));
+			ItemMeta meta = itemStack.getItemMeta();
+			meta.setDisplayName(ChatColor.GREEN + itemData);
+			
+			List<String> lore = meta.getLore();
+			String loreText = config.getString(config.getString("npc." + npcName + ".options." + itemData + ".lore"));
+			
+			try {
+				String[] words = loreText.split("\\s+");
+
+				int count = 0;
+				String sentence = "";
+				for (String word : words) {
+					sentence = sentence.concat(word + " ");
+					if (++count > 6) {
+						lore.add(sentence);
+						sentence = "";
+						count = 0;
+					}
+				}
+				lore.add(sentence);
+			} catch (NullPointerException e) {
+			}
+			
+			meta.setLore(lore);
+			itemStack.setItemMeta(meta);
+			inventoryScreen.addItem(itemStack);
+		}
+		return inventoryScreen;
 	}
 }
